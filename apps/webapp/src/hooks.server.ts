51b017e98c0fcee617ef11e031369cd7f81f8a0d
Thanish Nizam <mnmtanish@gmail.com>
2023-10-06 22:30:42 +0200

    feat: support user login with Github OAuth

---
+++ apps/webapp/src/hooks.server.ts
@@ -0,0 +1,23 @@
+import { SvelteKitAuth } from '@auth/sveltekit';
+import GitHub from '@auth/core/providers/github';
+import { AUTH_GITHUB_ID, AUTH_GITHUB_SECRET, AUTH_SECRET } from '$env/static/private';
+import { redirect, type Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+
+const authorization: Handle = async ({ event, resolve }) => {
+	if (!event.url.pathname.startsWith('/auth')) {
+		const session = await event.locals.getSession();
+		if (!session) {
+			throw redirect(303, '/auth');
+		}
+	}
+	return resolve(event);
+};
+
+const sveltekitauth = SvelteKitAuth({
+	providers: [GitHub({ clientId: AUTH_GITHUB_ID, clientSecret: AUTH_GITHUB_SECRET })],
+	secret: AUTH_SECRET,
+	trustHost: true
+});
+
+export const handle = sequence(sveltekitauth, authorization);
