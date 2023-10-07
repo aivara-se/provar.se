e4596cee39b9c733791dbf0558d1584e91065e74
Thanish Nizam <mnmtanish@gmail.com>
2023-10-07 13:34:27 +0200

    feat: allow users to log in with Google

--- apps/webapp/src/hooks.server.ts
+++ apps/webapp/src/hooks.server.ts
@@ -1,13 +1,20 @@
-import { AUTH_GITHUB_ID, AUTH_GITHUB_SECRET, AUTH_SECRET } from '$env/static/private';
+import {
+	AUTH_GITHUB_ID,
+	AUTH_GITHUB_SECRET,
+	AUTH_GOOGLE_ID,
+	AUTH_GOOGLE_SECRET,
+	AUTH_SECRET
+} from '$env/static/private';
 import { getMongoConnection } from '$lib/server/mongo';
 import GitHub from '@auth/core/providers/github';
+import Google from '@auth/core/providers/google';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import { SvelteKitAuth } from '@auth/sveltekit';
 import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
 const authorization: Handle = async ({ event, resolve }) => {
-	if (!event.url.pathname.startsWith('/auth')) {
+	if (!event.url.pathname.startsWith('/auth/')) {
 		const session = await event.locals.getSession();
 		if (!session) {
 			throw redirect(303, '/auth/login');
@@ -18,7 +25,18 @@ const authorization: Handle = async ({ event, resolve }) => {
 
 const sveltekitauth = SvelteKitAuth({
 	adapter: MongoDBAdapter(getMongoConnection()),
-	providers: [GitHub({ clientId: AUTH_GITHUB_ID, clientSecret: AUTH_GITHUB_SECRET })],
+	providers: [
+		Google({
+			clientId: AUTH_GOOGLE_ID,
+			clientSecret: AUTH_GOOGLE_SECRET,
+			allowDangerousEmailAccountLinking: true
+		}),
+		GitHub({
+			clientId: AUTH_GITHUB_ID,
+			clientSecret: AUTH_GITHUB_SECRET,
+			allowDangerousEmailAccountLinking: true
+		})
+	],
 	secret: AUTH_SECRET,
 	session: {
 		strategy: 'jwt'
--- apps/webapp/src/routes/auth/login/+page.svelte
+++ apps/webapp/src/routes/auth/login/+page.svelte
@@ -2,12 +2,21 @@
 	import { CenteredLayout } from '$lib/ui';
 	import { signIn } from '@auth/sveltekit/client';
 	import { Button } from 'flowbite-svelte';
-	import { GithubSolid } from 'flowbite-svelte-icons';
+	import { GithubSolid, GoogleSolid } from 'flowbite-svelte-icons';
 </script>
 
 <CenteredLayout>
-	<Button size="sm" color="dark" outline on:click={() => signIn('github', { callbackUrl: '/' })}>
-		<GithubSolid class="w-3 h-3 mr-1" />
-		Login with Github
-	</Button>
+	<div class="">
+		<Button size="sm" color="red" outline on:click={() => signIn('google', { callbackUrl: '/' })}>
+			<GoogleSolid class="w-3 h-3 mr-1" />
+			Login with Google
+		</Button>
+	</div>
+
+	<div class="mt-6">
+		<Button size="sm" color="dark" outline on:click={() => signIn('github', { callbackUrl: '/' })}>
+			<GithubSolid class="w-3 h-3 mr-1" />
+			Login with Github
+		</Button>
+	</div>
 </CenteredLayout>
