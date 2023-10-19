3e65f0e654a42089bdc05134aec607fcba999ab5
Nisma Nizam <fnismanizam@gmail.com>
2023-10-19 12:06:56 +0530

    feat: add organization id and findByOrganization

---
+++ apps/webapp/src/lib/server/features/feature.repository.ts
@@ -0,0 +1,66 @@
+import { getMongoClient } from '$lib/server/database';
+import { ObjectId, type Collection } from 'mongodb';
+import type { Feature } from './feature.types';
+
+/**
+ * The name of the MongoDB collection for features.
+ */
+const COLLECTION_NAME = 'feature';
+
+/**
+ * Type for the MongoDB document for Features in db.
+ */
+type FeatureDocument = {
+    _id: ObjectId;
+    name: string;
+    key: string;
+    organization: string[];
+};
+
+/**
+ * Convert a feature document from the db to Feature.
+ */
+function fromDocument(doc: FeatureDocument): Feature {
+    return {
+        id: doc._id.toHexString(),
+        name: doc.name,
+        key: doc.key,
+        organization: doc.organization,
+    };
+}
+
+/**
+ * Get the MongoDB collection for features.
+ */
+async function getCollection(): Promise<Collection<FeatureDocument>> {
+    const mongo = await getMongoClient();
+    return mongo.db().collection(COLLECTION_NAME);
+}
+
+/**
+ * Find a feature by id.
+ */
+export async function findById(id: string): Promise<Feature | null> {
+    const coll = await getCollection();
+    const doc = await coll.findOne({ _id: new ObjectId(id) });
+    return doc ? fromDocument(doc) : null;
+}
+
+/**
+ * Find a feature by key.
+ */
+export async function findByKey(key: string): Promise<Feature | null> {
+    const coll = await getCollection();
+    const doc = await coll.findOne({ key: new String(key) });
+    return doc ? fromDocument(doc) : null;
+}
+
+/**
+ * Find a feature by organisation
+ */
+export async function findByOrganisation(OrganizationId: string): Promise<Feature[]> {
+    const coll = await getCollection();
+    const doc = await coll.find({ organization: OrganizationId }).toArray();
+    return doc.map(fromDocument);
+}
+
---
+++ apps/webapp/src/lib/server/features/feature.types.ts
@@ -0,0 +1,9 @@
+/**
+ * Features.
+ */
+export interface Feature {
+    id: string;
+    name: string;
+    key: string;
+    organization: string[];
+}
