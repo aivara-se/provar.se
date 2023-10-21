291c479f2bd3f10632b54cb539decb05d9cbc19e
Thanish Nizam <mnmtanish@gmail.com>
2023-10-21 21:23:01 +0200

    test: add integration tests for the login page

--- apps/webapp/playwright.config.ts
+++ apps/webapp/playwright.config.ts
@@ -2,11 +2,11 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'yarn preview',
 		port: 4173
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /.+\.integration\.spec\.ts/
 };
 
 export default config;
