b082330f48396e992ee8cb89c2fa7f1fcb51b8dd
Thanish Nizam <mnmtanish@gmail.com>
2023-10-27 08:10:57 +0200

    chore: remove the text before the alert

--- apps/webapp/src/routes/org/[slug]/+page.svelte
+++ apps/webapp/src/routes/org/[slug]/+page.svelte
@@ -3,8 +3,6 @@
 	import { InfoCircleSolid } from 'flowbite-svelte-icons';
 </script>
 
-Dashboard
-
 <Alert color="blue" rounded={false} class="border-t-4">
 	<InfoCircleSolid slot="icon" class="w-4 h-4" />
 	<span class="font-medium">Alert!</span><br />
