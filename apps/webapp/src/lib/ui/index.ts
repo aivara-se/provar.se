1c8a2eed385619c30675211d152649d396b4e94b
Thanish Nizam <mnmtanish@gmail.com>
2023-10-27 08:05:54 +0200

    chore: move oneoff elements closer to pages

--- apps/webapp/src/lib/ui/index.ts
+++ apps/webapp/src/lib/ui/index.ts
@@ -1,5 +1,3 @@
 export { default as CenteredLayout } from './layouts/CenteredLayout.svelte';
 export { default as LSidebarLayout } from './layouts/LSidebarLayout.svelte';
-export { default as HorizontalSeparator } from './elements/HorizontalSeparator.svelte';
-export { default as SidebarNavigationMenu } from './oneoffs/SidebarNavigationMenu.svelte';
-export { default as OrganizationSelector } from './oneoffs/OrganizationSelector.svelte';
+export { default as HorizontalSeparator } from './components/HorizontalSeparator.svelte';
