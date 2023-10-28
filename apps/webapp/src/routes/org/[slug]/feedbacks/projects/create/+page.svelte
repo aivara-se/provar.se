c81de75c44b8fc66b1541f75c1d3ff87de90b0b1
Nisma Nizam <fnismanizam@gmail.com>
2023-10-28 12:08:56 +0530

    feat: create form and API to create project (TBC)

---
+++ apps/webapp/src/routes/org/[slug]/feedbacks/projects/create/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { applyAction, deserialize } from '$app/forms';
+	import { invalidateAll } from '$app/navigation';
+	import { CenteredLayout } from '$lib/ui';
+	import type { ActionResult } from '@sveltejs/kit';
+	import { Button, Input, Label } from 'flowbite-svelte';
+
+	let name = '';
+  // TODO: set organization Id
+  let organizationId = '';
+
+	async function submitForm(event: { currentTarget: EventTarget & HTMLFormElement }) {
+		const data = new FormData();
+		data.set('name', name);
+    data.set('organizationId', organizationId)
+		const response = await fetch(event.currentTarget.action, {
+			method: 'POST',
+			body: data
+		});
+		const result: ActionResult = deserialize(await response.text());
+		if (result.type === 'success') {
+			await invalidateAll();
+		}
+		applyAction(result);
+	}
+</script>
+
+<CenteredLayout>
+	<form method="POST" on:submit|preventDefault={submitForm}>
+		<div class="grid gap-6 mb-6">
+			<div>
+				<Label color="gray" for="name" class="mb-2">Project name</Label>
+				<Input type="text" id="name" required bind:value={name} />
+			</div>
+			<div>
+				<Button outline type="submit" size="sm">Create Project</Button>
+			</div>
+		</div>
+	</form>
+</CenteredLayout>
