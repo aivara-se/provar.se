822d779ef462f533d1851e33863abb88f7919d21
Nisma Nizam <fnismanizam@gmail.com>
2023-10-28 12:07:51 +0530

    chore: export project repository

--- apps/webapp/src/lib/server/index.ts
+++ apps/webapp/src/lib/server/index.ts
@@ -3,3 +3,6 @@ export type { User } from './user/user.types';
 
 export * as OrganizationRepository from './organization/organization.repository';
 export type { Organization } from './organization/organization.types';
+
+export * as ProjectRepository from './project/project.repository';
+export type { Project } from './project/project.types';
