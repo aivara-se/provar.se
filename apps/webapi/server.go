72d756ea7c46a7a8eb8ba17b0fc1bec4ad64334c
Thanish Nizam <mnmtanish@gmail.com>
2023-10-29 19:24:24 +0100

    feat: load environment variables from .env

--- apps/webapi/server.go
+++ apps/webapi/server.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"provar.se/webapi/feedback"
 	"provar.se/webapi/healthcheck"
 )
+
+func init() {
+    err := godotenv.Load()
+    if err != nil {
+      log.Fatal("Error loading .env file")
+    }
+}
 
 func main() {
     app := fiber.New(fiber.Config{
