75f4abe01fc5d1cb1bffd422b3701a9355cdf6c6
Thanish Nizam <mnmtanish@gmail.com>
2023-10-30 08:10:17 +0100

    feat: store feedback on API request

--- apps/webapi/routes/healthcheck/basic_healthcheck.go
+++ apps/webapi/routes/healthcheck/basic_healthcheck.go
@@ -2,10 +2,10 @@ package healthcheck
 
 import "github.com/gofiber/fiber/v2"
 
+// SetupBasicHealthcheck sets up the route for a basic healthcheck
+// GET /ping
 func SetupBasicHealthcheck(app *fiber.App) {
-	app.Get("/ping", handler)
-}
-
-func handler(c *fiber.Ctx) error {
-	return c.SendString("pong")
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.SendString("pong")
+	})
 }
