8e4ac5ee447c248f726742d6467db19ca1570654
Thanish Nizam <mnmtanish@gmail.com>
2023-11-05 08:09:22 +0100

    feat: add side nav icons and more actions

--- apps/webapp/src/routes/org/[slug]/+layout.svelte
+++ apps/webapp/src/routes/org/[slug]/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { LSidebarLayout, HorizontalSeparator } from '$lib/ui';
+	import { HorizontalSeparator, LSidebarLayout } from '$lib/ui';
+	import { Button, ButtonGroup } from 'flowbite-svelte';
+	import {
+		AdjustmentsHorizontalOutline,
+		UserSettingsOutline,
+		MessageDotsOutline,
+		ChartMixedOutline,
+		MessagesOutline
+	} from 'flowbite-svelte-icons';
+	import type { LayoutData } from './$types';
 	import OrganizationSelector from './_components/OrganizationSelector.svelte';
 	import SidebarNavigationMenu from './_components/SidebarNavigationMenu.svelte';
-	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
 
@@ -11,8 +19,8 @@
 	 * Navigation items
 	 */
 	$: navigationItems = [
-		{ name: 'Dashboard', href: '/org/' + $page.params.slug },
-		{ name: 'Feedbacks', href: '/org/' + $page.params.slug + '/feedbacks' }
+		{ name: 'Dashboard', href: `/org/${$page.params.slug}`, icon: ChartMixedOutline },
+		{ name: 'Feedbacks', href: `/org/${$page.params.slug}/feedbacks`, icon: MessagesOutline }
 	].map((item) => ({
 		...item,
 		active: item.href === $page.url.pathname
@@ -26,7 +34,19 @@
 		<SidebarNavigationMenu items={navigationItems} />
 	</svelte:fragment>
 
-	<svelte:fragment slot="sidebar-bottom">logo</svelte:fragment>
+	<svelte:fragment slot="sidebar-bottom">
+		<ButtonGroup>
+			<Button>
+				<UserSettingsOutline class="w-4 h-4 mr-1" />
+			</Button>
+			<Button href={`/org/${$page.params.slug}/preferences`}>
+				<AdjustmentsHorizontalOutline class="w-4 h-4 mr-1" />
+			</Button>
+			<Button>
+				<MessageDotsOutline class="w-4 h-4 mr-1" />
+			</Button>
+		</ButtonGroup>
+	</svelte:fragment>
 
 	<svelte:fragment>
 		<slot />
