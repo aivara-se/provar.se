f4fe0fbf4fb572496ddd6f9921d01be389235018
Thanish Nizam <mnmtanish@gmail.com>
2023-11-05 08:45:39 +0100

    feat: use tabbed layout for org settings

---
+++ apps/webapp/src/routes/org/[slug]/preferences/_tab-content/MembersTabContent.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Alert } from 'flowbite-svelte';
+	import { InfoCircleSolid } from 'flowbite-svelte-icons';
+</script>
+
+<Alert color="blue" rounded={false} class="border-t-4">
+	<InfoCircleSolid slot="icon" class="w-4 h-4" />
+	<span class="font-medium">Members</span><br />
+	This page is under construction.
+</Alert>
