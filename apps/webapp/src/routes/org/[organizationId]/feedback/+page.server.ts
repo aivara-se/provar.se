485818ec2a58b00263b5d5cb9500744b2c342e3f
Nisma Nizam <fnismanizam@gmail.com>
2023-11-11 16:00:43 +0530

    feat: display project list

---
+++ apps/webapp/src/routes/org/[organizationId]/feedback/+page.server.ts
@@ -0,0 +1,24 @@
+import { ProjectRepository, OrganizationRepository, type Organization } from '$lib/server';
+import type { Session } from '@auth/core/types';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+/**
+ * Loads data required for all tabs on organization preferences page.
+ */
+export const load: PageServerLoad = async ({ locals, params }) => {
+	const session = (await locals.getSession()) as Session;
+	const organization = await OrganizationRepository.findById(params.organizationId);
+	if (!organization || isMember(organization, session.user.id)) {
+		throw error(403);
+	}
+	const projects = await ProjectRepository.findByOrganization(organization.id);
+	return { projects: projects };
+};
+
+/**
+ * Asserts that the user is a member of the organization.
+ */
+function isMember(organization: Organization, userId: string): boolean {
+	return !organization?.members.includes(userId);
+}
--- apps/webapp/src/routes/org/[organizationId]/feedback/+page.svelte
+++ apps/webapp/src/routes/org/[organizationId]/feedback/+page.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
-  import { Button } from 'flowbite-svelte';
+	import { Button } from 'flowbite-svelte';
 	import { page } from '$app/stores';
+	import type { Project } from '$lib/types';
 
-	$: createLink = `/org/${$page.params.organizationId}/feedback/project/create`
+	$: createLink = `/org/${$page.params.organizationId}/feedback/project/create`;
+
+	function transformToListItem(project: Project) {
+		return {
+			name: project.name,
+			link: `/org/${project.organizationId}/feedback/project/${project.id}`
+		};
+	}
+
+	$: projectListItems = $page.data.projects.map(transformToListItem);
 </script>
 
-<Button href={createLink}>
-	Create Project
-</Button>
+<Button href={createLink}>Create Project</Button>
+
+<ul>
+	{#each projectListItems as project}
+		<li>
+			<a href={project.link}>{project.name}</a>
+		</li>
+	{/each}
+</ul>
