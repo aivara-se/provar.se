e8a4b922052140525dd479141c47edbaf3af36ff
Thanish Nizam <mnmtanish@gmail.com>
2023-11-12 17:07:02 +0100

    chore: reduce db queries by using parent data

--- apps/webapp/src/routes/+page.server.ts
+++ apps/webapp/src/routes/+page.server.ts
@@ -1,11 +1,8 @@
-import { OrganizationRepository } from '$lib/server';
-import type { Session } from '@auth/core/types';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-	const session = (await event.locals.getSession()) as Session;
-	const organizations = await OrganizationRepository.findByMember(session.user.id);
+	const { organizations } = await event.parent();
 	if (organizations.length === 0) {
 		throw redirect(302, `/org/create`);
 	}
