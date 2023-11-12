fc28026595c55d04340ca88fe9e683adaaded96e
Thanish Nizam <mnmtanish@gmail.com>
2023-11-12 17:07:02 +0100

    chore: reduce db queries by using parent data

--- apps/webapp/src/routes/+layout.server.ts
+++ apps/webapp/src/routes/+layout.server.ts
@@ -1,7 +1,9 @@
+import { OrganizationRepository } from '$lib/server';
+import type { Session } from '@auth/core/types';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
-	return {
-		session: await event.locals.getSession()
-	};
+	const session = (await event.locals.getSession()) as Session;
+	const organizations = await OrganizationRepository.findByMember(session.user.id);
+	return { session, organizations };
 };
