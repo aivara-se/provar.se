62837041e76d2a0ebacd3395b41b3329fc9b1d3b
Thanish Nizam <mnmtanish@gmail.com>
2023-11-26 01:08:43 +0100

    feat: export feedback repository in barrel

--- apps/webapp/src/lib/server/index.ts
+++ apps/webapp/src/lib/server/index.ts
@@ -2,4 +2,5 @@ export * as UserRepository from './user/user.repository';
 export * as OrganizationRepository from './organization/organization.repository';
 export * as ProjectRepository from './project/project.repository';
 export * as CredentialRepository from './credential/credential.repository';
+export * as FeedbackRepository from './feedback/feedback.repository';
 export * from './action-utils/get-selected-organization';
