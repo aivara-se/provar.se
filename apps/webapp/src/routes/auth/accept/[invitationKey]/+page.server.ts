bbdf3779ad0758bd74ba2ade0540fbf6d0c94e33
Thanish Nizam <mnmtanish@gmail.com>
2023-12-16 07:23:08 +0100

    feat: load invitation details ona ccept page

---
+++ apps/webapp/src/routes/auth/accept/[invitationKey]/+page.server.ts
@@ -0,0 +1,21 @@
+import { error, redirect } from '@sveltejs/kit';
+import { InvitationRepository, InvitationService, OrganizationRepository } from '$lib/server';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async (event) => {
+	const session = await event.locals.getSession();
+	if (!session || !session.user?.id) {
+		throw redirect(302, `/auth/login`);
+	}
+	const invitationKey = event.params.invitationKey;
+	const invitation = await InvitationRepository.findByKey(invitationKey);
+	if (!invitation) {
+		throw error(404, 'Invitation not found');
+	}
+	const canAccept = await InvitationService.canAccept(invitationKey, session.user?.id);
+	if (!canAccept) {
+		throw error(403, 'User cannot accept this invitation');
+	}
+	const organization = await OrganizationRepository.findById(invitation.organizationId);
+	return { invitation, organizationName: organization?.name };
+};
