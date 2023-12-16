fd601286af810a7c2ebe26aff5beea47a3a9082d
Thanish Nizam <mnmtanish@gmail.com>
2023-12-16 08:38:41 +0100

    feat: add placeholder page for accepting an invitation

---
+++ apps/webapp/src/routes/auth/accept/[invitationKey]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { CenteredLayout } from '$lib/ui';
+	import { Button } from 'flowbite-svelte';
+
+	async function acceptInvitation() {
+		console.log('acceptInvitation');
+	}
+</script>
+
+<CenteredLayout>
+	<Button size="sm" color="red" outline on:click={() => acceptInvitation()}>
+		Accept Invitation
+	</Button>
+</CenteredLayout>
