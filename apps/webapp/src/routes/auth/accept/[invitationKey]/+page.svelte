60e43520f6ddebd106e6ce68c77bc857d49972d5
Thanish Nizam <mnmtanish@gmail.com>
2023-12-31 11:30:20 +0100

    chore: refactor accept invitation form

--- apps/webapp/src/routes/auth/accept/[invitationKey]/+page.svelte
+++ apps/webapp/src/routes/auth/accept/[invitationKey]/+page.svelte
@@ -1,24 +1,8 @@
 <script lang="ts">
-	import { applyAction, deserialize } from '$app/forms';
-	import { invalidateAll } from '$app/navigation';
 	import { CenteredLayout } from '$lib/client/ui';
-	import type { ActionResult } from '@sveltejs/kit';
-	import { Button } from 'flowbite-svelte';
-
-	async function acceptInvitation() {
-		const data = new FormData();
-		const response = await fetch('', {
-			method: 'post',
-			body: data
-		});
-		const result: ActionResult = deserialize(await response.text());
-		if (result.type === 'success') {
-			await invalidateAll();
-		}
-		applyAction(result);
-	}
+	import AcceptInvitationForm from './_components/AcceptInvitationForm.svelte';
 </script>
 
 <CenteredLayout>
-	<Button size="sm" color="red" outline on:click={acceptInvitation}>Accept Invitation</Button>
+	<AcceptInvitationForm />
 </CenteredLayout>
