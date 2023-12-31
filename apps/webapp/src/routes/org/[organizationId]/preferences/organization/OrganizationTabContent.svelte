5ea357e13226b7e8006ca89a0affe5dca0fb03eb
Thanish Nizam <mnmtanish@gmail.com>
2023-12-31 12:01:05 +0100

    chore: refactor organization preferences tab

--- apps/webapp/src/routes/org/[organizationId]/preferences/organization/OrganizationTabContent.svelte
+++ apps/webapp/src/routes/org/[organizationId]/preferences/organization/OrganizationTabContent.svelte
@@ -3,55 +3,44 @@
 	import { invalidateAll } from '$app/navigation';
 	import { page } from '$app/stores';
 	import type { ActionResult } from '@sveltejs/kit';
-	import { Alert, Button, Heading, Input, Label, Modal, P, Textarea } from 'flowbite-svelte';
+	import { Alert, Button, Heading, Input, Label, P, Textarea } from 'flowbite-svelte';
 	import { ExclamationCircleOutline } from 'flowbite-svelte-icons';
-
-	let organization = $page.data.organization;
-	let name = organization?.name || '';
-	let description = organization?.description || '';
+	import DeleteOrganizationModal from './_components/DeleteOrganizationModal.svelte';
+	import LeaveOrganizationModal from './_components/LeaveOrganizationModal.svelte';
 
 	let isLeaveModalOpen = false;
 	let isDeleteModalOpen = false;
 
+	interface Fields {
+		name: string;
+		description: string;
+	}
+
+	let fields: Fields = {
+		name: $page.data.organization.name || '',
+		description: $page.data.organization.description || ''
+	};
+
+	function resetFields() {
+		fields = {
+			name: $page.data.organization.name || '',
+			description: $page.data.organization.description || ''
+		};
+	}
+
 	async function updateOrganization() {
 		const data = new FormData();
-		data.set('name', name);
-		data.set('description', description);
-		const response = await fetch('?/updateOrganization', {
-			method: 'post',
-			body: data
-		});
+		for (const [key, value] of Object.entries(fields)) {
+			data.set(key, value);
+		}
+		const action = '?/updateOrganization';
+		const response = await fetch(action, { method: 'post', body: data });
 		const result: ActionResult = deserialize(await response.text());
 		if (result.type === 'success') {
 			await invalidateAll();
 		}
 		applyAction(result);
-	}
-
-	async function leaveOrganization() {
-		const data = new FormData();
-		const response = await fetch('?/leaveOrganization', {
-			method: 'post',
-			body: data
-		});
-		const result: ActionResult = deserialize(await response.text());
-		if (result.type === 'success') {
-			await invalidateAll();
-		}
-		applyAction(result);
-	}
-
-	async function deleteOrganization() {
-		const data = new FormData();
-		const response = await fetch('?/deleteOrganization', {
-			method: 'post',
-			body: data
-		});
-		const result: ActionResult = deserialize(await response.text());
-		if (result.type === 'success') {
-			await invalidateAll();
-		}
-		applyAction(result);
+		resetFields();
 	}
 </script>
 
@@ -62,74 +51,57 @@
 	used by AI features.
 </P>
 
-<div class="mt-6">
-	<Label for="name" class="block mb-2">Name:</Label>
-	<Input id="name" required bind:value={name} />
-</div>
-
-<div class="mt-6">
-	<Label for="description" class="block mb-2">Description:</Label>
-	<Textarea id="description" required bind:value={description} />
-</div>
-
-<Alert color="red" class="mt-6">
-	<div class="flex items-center gap-3">
-		<ExclamationCircleOutline slot="icon" class="w-4 h-4" />
-		<span class="font-medium">Leave the organization</span>
+<form on:submit|self|stopPropagation|preventDefault={updateOrganization}>
+	<div class="mt-6">
+		<Label for="name" class="block mb-2">Name:</Label>
+		<Input id="name" required bind:value={fields.name} />
 	</div>
-	<p class="mt-2 mb-2 text-sm">
-		Exiting this organization will revoke your access to its feedback data and associated resources.
-		You will no longer be able to view or manage feedback within this organization. If you are the
-		last member in this organization, the organization and all its feedback data will be deleted
-		permanently. This action cannot be reversed.
-	</p>
-	<div class="flex gap-2">
-		<Button size="xs" color="red" outline on:click={() => (isLeaveModalOpen = true)}>
-			Leave "{name}"
-		</Button>
-	</div>
-</Alert>
 
-<Alert color="red" class="mt-6">
-	<div class="flex items-center gap-3">
-		<ExclamationCircleOutline slot="icon" class="w-4 h-4" />
-		<span class="font-medium">Delete the organization</span>
+	<div class="mt-6">
+		<Label for="description" class="block mb-2">Description:</Label>
+		<Textarea id="description" required bind:value={fields.description} />
 	</div>
-	<p class="mt-2 mb-4 text-sm">
-		Deleting this organization will permanently erase all collected feedback data, including
-		associated records and information. Proceeding with this action cannot be undone. Please ensure
-		that you have backed up any critical data or exported necessary information before deleting the
-		organization. This action cannot be reversed.
-	</p>
-	<div class="flex gap-2">
-		<Button size="xs" color="red" outline on:click={() => (isDeleteModalOpen = true)}>
-			Delete "{name}"
-		</Button>
-	</div>
-</Alert>
 
-<div class="flex justify-end mt-6">
-	<Button size="sm" color="primary" on:click={updateOrganization}>Update</Button>
-</div>
+	<Alert color="red" class="mt-6">
+		<div class="flex items-center gap-3">
+			<ExclamationCircleOutline slot="icon" class="w-4 h-4" />
+			<span class="font-medium">Leave the organization</span>
+		</div>
+		<p class="mt-2 mb-2 text-sm">
+			Exiting this organization will revoke your access to its feedback data and associated
+			resources. You will no longer be able to view or manage feedback within this organization. If
+			you are the last member in this organization, the organization and all its feedback data will
+			be deleted permanently. This action cannot be reversed.
+		</p>
+		<div class="flex gap-2">
+			<Button size="xs" color="red" outline on:click={() => (isLeaveModalOpen = true)}>
+				Leave "{$page.data.organization.name}"
+			</Button>
+		</div>
+	</Alert>
 
-<Modal bind:open={isLeaveModalOpen} size="sm" autoclose>
-	<div class="text-center">
-		<ExclamationCircleOutline class="mx-auto mb-4 text-gray-500 w-8 h-8" />
-		<h3 class="mb-5 text-lg font-normal text-gray-800">
-			Are you sure you want to leave "{name}"?
-		</h3>
-		<Button color="red" class="me-2" on:click={leaveOrganization}>Yes, I'm sure</Button>
-		<Button color="alternative">No, cancel</Button>
-	</div>
-</Modal>
+	<Alert color="red" class="mt-6">
+		<div class="flex items-center gap-3">
+			<ExclamationCircleOutline slot="icon" class="w-4 h-4" />
+			<span class="font-medium">Delete the organization</span>
+		</div>
+		<p class="mt-2 mb-4 text-sm">
+			Deleting this organization will permanently erase all collected feedback data, including
+			associated records and information. Proceeding with this action cannot be undone. Please
+			ensure that you have backed up any critical data or exported necessary information before
+			deleting the organization. This action cannot be reversed.
+		</p>
+		<div class="flex gap-2">
+			<Button size="xs" color="red" outline on:click={() => (isDeleteModalOpen = true)}>
+				Delete "{$page.data.organization.name}"
+			</Button>
+		</div>
+	</Alert>
 
-<Modal bind:open={isDeleteModalOpen} size="sm" autoclose>
-	<div class="text-center">
-		<ExclamationCircleOutline class="mx-auto mb-4 text-gray-500 w-8 h-8" />
-		<h3 class="mb-5 text-lg font-normal text-gray-800">
-			Are you sure you want to delete "{name}"?
-		</h3>
-		<Button color="red" class="me-2" on:click={deleteOrganization}>Yes, I'm sure</Button>
-		<Button color="alternative">No, cancel</Button>
+	<div class="flex justify-end mt-6">
+		<Button type="submit" size="sm" color="primary">Update</Button>
 	</div>
-</Modal>
+</form>
+
+<LeaveOrganizationModal organization={$page.data.organization} bind:isOpen={isLeaveModalOpen} />
+<DeleteOrganizationModal organization={$page.data.organization} bind:isOpen={isDeleteModalOpen} />
