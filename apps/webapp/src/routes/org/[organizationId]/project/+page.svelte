22e94698d4bdb31d65dc113229c71d168fd34972
Thanish Nizam <mnmtanish@gmail.com>
2023-12-31 11:32:23 +0100

    chore: refactor project action modals

--- apps/webapp/src/routes/org/[organizationId]/project/+page.svelte
+++ apps/webapp/src/routes/org/[organizationId]/project/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-	import { applyAction, deserialize } from '$app/forms';
-	import { invalidateAll } from '$app/navigation';
 	import { page } from '$app/stores';
 	import type { Project } from '$lib/types';
-	import type { ActionResult } from '@sveltejs/kit';
 	import {
 		Breadcrumb,
 		BreadcrumbItem,
@@ -12,12 +9,11 @@
 		Dropdown,
 		DropdownItem,
 		Heading,
-		Input,
-		Label,
-		Modal,
 		Progressbar
 	} from 'flowbite-svelte';
-	import { DotsHorizontalOutline, ExclamationCircleOutline } from 'flowbite-svelte-icons';
+	import { DotsHorizontalOutline } from 'flowbite-svelte-icons';
+	import CreateProjectModel from './_components/CreateProjectModel.svelte';
+	import DeleteProjectModal from './_components/DeleteProjectModal.svelte';
 
 	$: projects = $page.data.projects || [];
 	$: projectListItems = projects.map((project: Project) => ({
@@ -31,40 +27,9 @@
 
 	let projectToDelete: Project;
 
-	let name = '';
-
 	function selectProjectToDelete(project: Project) {
 		projectToDelete = project;
 		isDeleteModalOpen = true;
-	}
-
-	async function createProject() {
-		const data = new FormData();
-		data.set('name', name);
-		const action = `/org/${$page.params.organizationId}/project?/createProject`;
-		const response = await fetch(action, {
-			method: 'POST',
-			body: data
-		});
-		const result: ActionResult = deserialize(await response.text());
-		if (result.type === 'success') {
-			await invalidateAll();
-		}
-		applyAction(result);
-	}
-
-	async function deleteProject(projectId: string) {
-		const data = new FormData();
-		const action = `/org/${$page.params.organizationId}/project/${projectId}?/delete`;
-		const response = await fetch(action, {
-			method: 'POST',
-			body: data
-		});
-		const result: ActionResult = deserialize(await response.text());
-		if (result.type === 'success') {
-			await invalidateAll();
-		}
-		applyAction(result);
 	}
 </script>
 
@@ -103,26 +68,8 @@
 	</Button>
 </section>
 
-<Modal title="Create Project" bind:open={isCreateModalOpen} autoclose>
-	<Label for="name" class="block mb-2">Name:</Label>
-	<Input id="name" required bind:value={name} />
-	<svelte:fragment slot="footer">
-		<Button size="sm" color="primary" on:click={createProject}>Create</Button>
-	</svelte:fragment>
-</Modal>
-
-<Modal bind:open={isDeleteModalOpen} size="sm" autoclose>
-	<div class="text-center">
-		<ExclamationCircleOutline class="mx-auto mb-4 text-gray-500 w-8 h-8" />
-		<h3 class="mb-5 text-lg font-normal text-gray-800">
-			Are you sure you want to delete "{projectToDelete.name}"?
-		</h3>
-		<Button color="red" class="me-2" on:click={() => deleteProject(projectToDelete.id)}>
-			Yes, I'm sure
-		</Button>
-		<Button color="alternative">No, cancel</Button>
-	</div>
-</Modal>
+<CreateProjectModel bind:isOpen={isCreateModalOpen} />
+<DeleteProjectModal bind:isOpen={isDeleteModalOpen} project={projectToDelete} />
 
 <style>
 	.project-item {
