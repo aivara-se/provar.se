885717f373fc38e944b9623eb8a5b7455cbbb3cc
Thanish Nizam <mnmtanish@gmail.com>
2024-01-01 15:47:24 +0100

    feat: sort feedback tags alphabetically

--- apps/webapp/src/routes/org/[organizationId]/feedback/(detail)/FeedbackDetail.svelte
+++ apps/webapp/src/routes/org/[organizationId]/feedback/(detail)/FeedbackDetail.svelte
@@ -36,10 +36,10 @@
 				</section>
 
 				<section class="mt-3 flex gap-2">
-					{#each Object.keys(feedback.tags || {}) as tagName}
+					{#each Object.keys(feedback.tags || {}).sort() as key}
 						<Badge border color="dark" class="border-gray-300 text-gray-600">
 							<TagOutline class="w-2.5 h-2.5 me-1.5" />
-							{tagName}: {feedback.tags?.[tagName]}
+							{key}: {feedback.tags?.[key]}
 						</Badge>
 					{/each}
 				</section>
