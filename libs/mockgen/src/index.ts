1e87b6fc5a3ad59a95e57fae7c738222ce3fb0ba
Thanish Nizam <mnmtanish@gmail.com>
2024-01-01 09:59:47 +0100

    feat: generate random feedback tags

--- libs/mockgen/src/index.ts
+++ libs/mockgen/src/index.ts
@@ -1,2 +1,41 @@
+import { generateCNPSFeedback } from './generators/cnps.js';
+import { generateCSATFeedback } from './generators/csat.js';
+import { generateRandomDate } from './generators/date.js';
+import { generateMetadata } from './generators/meta.js';
+import { generateRandomTags } from './generators/tags.js';
+import { generateTextFeedback } from './generators/text.js';
+import { generateUserdata } from './generators/user.js';
+
+/**
+ * Export helper function to generate the default range
+ */
 export { getDefaultPeriod } from './generators/time.js';
-export { generateFeedback, FeedbackType } from './generators/index.js';
+
+/**
+ * Feedback types
+ */
+export type FeedbackType = 'text' | 'csat' | 'cnps';
+
+/**
+ * Map of feedback types => feedback data generators
+ */
+const FeedbackGeneratorsMap = {
+	text: generateTextFeedback,
+	csat: generateCSATFeedback,
+	cnps: generateCNPSFeedback
+};
+
+/**
+ * Generates a feedback object
+ */
+export function generateFeedback(type: FeedbackType, period: [Date, Date]) {
+	const [start, end] = period;
+	return {
+		type,
+		time: generateRandomDate(start, end),
+		data: FeedbackGeneratorsMap[type](),
+		meta: Math.random() > 0.2 ? generateMetadata() : {},
+		user: Math.random() > 0.2 ? generateUserdata() : {},
+		tags: Math.random() > 0.2 ? generateRandomTags() : {}
+	};
+}
