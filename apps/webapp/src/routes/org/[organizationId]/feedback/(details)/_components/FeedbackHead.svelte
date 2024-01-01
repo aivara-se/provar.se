26e2a2288464a756f53e98f3ce5ebc5f281dfa44
Thanish Nizam <mnmtanish@gmail.com>
2024-01-01 17:45:41 +0100

    feat: add nps and csat values

---
+++ apps/webapp/src/routes/org/[organizationId]/feedback/(details)/_components/FeedbackHead.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Feedback } from '$lib/types';
+	import { P } from 'flowbite-svelte';
+
+	export let feedback: Feedback;
+</script>
+
+<section class="flex items-center justify-between">
+	<P size="xs" class="mt-2 text-gray-500 font-semibold">
+		Created: {feedback.createdAt.toLocaleDateString()} at {feedback.createdAt.toLocaleTimeString()}
+	</P>
+	<P size="xs" class="mt-2 text-gray-500 font-semibold">
+		{feedback.type === 'cnps' ? 'NPS' : feedback.type === 'csat' ? 'CSAT' : 'Text'}
+	</P>
+</section>
