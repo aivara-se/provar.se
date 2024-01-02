47aa92575df7f1463732010998d401083eca2c82
Thanish Nizam <mnmtanish@gmail.com>
2024-01-02 02:49:17 +0100

    feat: filter by feedback metadata

--- apps/webapp/src/routes/org/[organizationId]/feedback/(details)/_components/FeedbackMeta.svelte
+++ apps/webapp/src/routes/org/[organizationId]/feedback/(details)/_components/FeedbackMeta.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { mergeSearchValue } from '$lib/client/search';
 	import type { Feedback } from '$lib/types';
 	import {
 		Accordion,
@@ -10,8 +11,13 @@
 		TableHead,
 		TableHeadCell
 	} from 'flowbite-svelte';
+	import { FilterOutline } from 'flowbite-svelte-icons';
 
 	export let feedback: Feedback;
+
+	function filterByMetadata(key: string, val: unknown) {
+		mergeSearchValue({ meta: { [key]: val } });
+	}
 </script>
 
 <section class="mt-3">
@@ -24,6 +30,7 @@
 				<TableHead>
 					<TableHeadCell>Key</TableHeadCell>
 					<TableHeadCell>Value</TableHeadCell>
+					<TableHeadCell></TableHeadCell>
 				</TableHead>
 				<TableBody>
 					{#each Object.keys(feedback.meta || {}).sort() as key}
@@ -33,6 +40,12 @@
 							</TableBodyCell>
 							<TableBodyCell tdClass="px-6 py-4 font-normal">
 								{feedback.meta?.[key]}
+							</TableBodyCell>
+							<TableBodyCell tdClass="px-6 py-4 font-normal">
+								<FilterOutline
+									class="w-3 h-3 text-gray-500 cursor-pointer"
+									on:click={() => filterByMetadata(key, feedback.meta?.[key])}
+								/>
 							</TableBodyCell>
 						</TableBodyRow>
 					{/each}
