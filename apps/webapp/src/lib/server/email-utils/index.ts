dc4a481cd01aac3b5fdd7c19b221faf3bcb3d6e4
Thanish Nizam <mnmtanish@gmail.com>
2024-02-03 17:10:18 +0100

    fix: fix ERR_MODULE_NOT_FOUND error on webapp

--- apps/webapp/src/lib/server/email-utils/index.ts
+++ apps/webapp/src/lib/server/email-utils/index.ts
@@ -1,4 +1,5 @@
-export * from './email.types';
+export { EmailProvider } from './email.provider';
 export * as EmailService from './email.service';
+export * from './email.types';
 export * as InvitationTemplate from './templates/invitation.tpl';
 export * as VerifyLoginTemplate from './templates/verifylogin.tpl';
