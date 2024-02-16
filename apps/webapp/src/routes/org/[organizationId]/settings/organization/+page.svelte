0d28a66fefa88bf51a45be0b5127c12a2f01ba62
Thanish Nizam <mnmtanish@gmail.com>
2024-02-16 23:34:11 +0100

    feat: confirm leave and delete actions

--- apps/webapp/src/routes/org/[organizationId]/settings/organization/+page.svelte
+++ apps/webapp/src/routes/org/[organizationId]/settings/organization/+page.svelte
@@ -2,8 +2,12 @@
 	import { AlertCircleIcon } from 'lucide-svelte';
 	import { enhance } from '$app/forms';
 	import UpdateOrganizationForm from './(forms)/UpdateOrganizationForm.svelte';
+	import { CloseButton } from '$lib/client/forms';
 
 	export let data;
+
+	let isLeaveModalOpen = false;
+	let isDeleteModalOpen = false;
 </script>
 
 <UpdateOrganizationForm formData={data.UpdateOrganizationForm} />
@@ -19,9 +23,22 @@
 		last member in this organization, the organization and all its feedback data will be deleted
 		permanently. This action cannot be reversed.
 	</p>
-	<form method="POST" action="?/leaveOrganization" use:enhance>
-		<button class="btn btn-sm btn-neutral mt-4">Leave "{data.organization?.name}"</button>
-	</form>
+	<button class="btn btn-sm btn-neutral mt-4" on:click={() => (isLeaveModalOpen = true)}>
+		Leave {data.organization?.name}
+	</button>
+	<dialog class="modal" class:modal-open={isLeaveModalOpen}>
+		<div class="modal-box flex flex-col items-center">
+			<CloseButton on:click={() => (isLeaveModalOpen = false)} />
+			<AlertCircleIcon class="w-8 h-8" />
+			<p class="py-4 text-center">Are you sure you want to leave {data.organization?.name}?</p>
+			<footer class="flex justify-around gap-4">
+				<form method="POST" action="?/leaveOrganization" use:enhance>
+					<button class="btn btn-neutral">Leave {data.organization?.name}</button>
+				</form>
+				<button class="btn btn-ghost" on:click={() => (isLeaveModalOpen = false)}> Cancel </button>
+			</footer>
+		</div>
+	</dialog>
 </section>
 
 <section class="mt-8 rounded-lg p-4 bg-gray-100 text-gray-900 dark:bg-gray-950 dark:text-gray-200">
@@ -35,7 +52,20 @@
 		that you have backed up any critical data or exported necessary information before deleting the
 		organization. This action cannot be reversed.
 	</p>
-	<form method="POST" action="?/deleteOrganization" use:enhance>
-		<button class="btn btn-sm btn-neutral mt-4">Delete "{data.organization?.name}"</button>
-	</form>
+	<button class="btn btn-sm btn-neutral mt-4" on:click={() => (isDeleteModalOpen = true)}>
+		Delete {data.organization?.name}
+	</button>
+	<dialog class="modal" class:modal-open={isDeleteModalOpen}>
+		<div class="modal-box flex flex-col items-center">
+			<CloseButton on:click={() => (isDeleteModalOpen = false)} />
+			<AlertCircleIcon class="w-8 h-8" />
+			<p class="py-4 text-center">Are you sure you want to delete {data.organization?.name}?</p>
+			<footer class="flex justify-around gap-4">
+				<form method="POST" action="?/deleteOrganization" use:enhance>
+					<button class="btn btn-neutral">Delete {data.organization?.name}</button>
+				</form>
+				<button class="btn btn-ghost" on:click={() => (isDeleteModalOpen = false)}> Cancel </button>
+			</footer>
+		</div>
+	</dialog>
 </section>
