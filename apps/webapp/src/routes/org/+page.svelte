d5247e5e1b286c2899d8d21e7feab793bc09794e
Thanish Nizam <mnmtanish@gmail.com>
2024-02-16 20:27:11 +0100

    feat: refactor create organization form

--- apps/webapp/src/routes/org/+page.svelte
+++ apps/webapp/src/routes/org/+page.svelte
@@ -10,11 +10,11 @@
 	<div class="flex flex-col w-80 gap-4">
 		<h1 class="text-lg font-medium antialiased">Organizations</h1>
 		{#each organizations as organization}
-			<a href="/org/{organization.id}" class="btn btn-md btn-block btn-neutral"
-				>{organization.name}</a
-			>
+			<a href="/org/{organization.id}" class="btn btn-block rounded-lg">
+				{organization.name}
+			</a>
 		{/each}
 		<div class="divider">or</div>
-		<a href="/org/create" class="btn btn-md btn-block btn-ghost">Create an organization</a>
+		<a href="/org/create" class="btn btn-block btn-ghost">Create an organization</a>
 	</div>
 </AuthLayout>
