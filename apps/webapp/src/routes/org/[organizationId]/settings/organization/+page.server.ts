5ad654cb4d448c31f18f578d3c762dbdde6473d9
Thanish Nizam <mnmtanish@gmail.com>
2024-02-16 22:22:38 +0100

    feat: add buttons to leave and delete organizations

--- apps/webapp/src/routes/org/[organizationId]/settings/organization/+page.server.ts
+++ apps/webapp/src/routes/org/[organizationId]/settings/organization/+page.server.ts
@@ -1,5 +1,7 @@
 import { getSelectedOrganization } from '$lib/server/action-utils';
-import { OrganizationRepository } from '$lib/server/organization';
+import { OrganizationRepository, OrganizationService } from '$lib/server/organization';
+import type { Session } from '@auth/core/types';
+import { redirect } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import type { Actions, PageServerLoad } from './$types';
@@ -21,5 +23,24 @@ export const actions: Actions = {
 		const form = await superValidate(event.request, zod(schema));
 		const { name, description } = form.data;
 		await OrganizationRepository.update(organization.id, { name, description });
+	},
+
+	/**
+	 * Leaves the organization and delete the organization if there are no other members.
+	 */
+	leaveOrganization: async (event) => {
+		const session = (await event.locals.getSession()) as Session;
+		const organization = await getSelectedOrganization(event);
+		await OrganizationService.removeMember(organization.id, session.user.id);
+		redirect(302, '/');
+	},
+
+	/**
+	 * Removes the organization and all associated data.
+	 */
+	deleteOrganization: async (event) => {
+		const organization = await getSelectedOrganization(event);
+		await OrganizationService.remove(organization.id);
+		redirect(302, '/');
 	}
 };
--- apps/webapp/src/routes/org/[organizationId]/settings/organization/+page.svelte
+++ apps/webapp/src/routes/org/[organizationId]/settings/organization/+page.svelte
@@ -1,7 +1,41 @@
 <script lang="ts">
+	import { AlertCircleIcon } from 'lucide-svelte';
+	import { enhance } from '$app/forms';
 	import UpdateOrganizationForm from './(forms)/UpdateOrganizationForm.svelte';
 
 	export let data;
 </script>
 
 <UpdateOrganizationForm formData={data.UpdateOrganizationForm} />
+
+<section class="mt-8 rounded-lg p-4 bg-gray-100 text-gray-900 dark:bg-gray-950 dark:text-gray-200">
+	<h3 class="flex items-center justify-between text-lg font-medium">
+		Leave the organization!
+		<AlertCircleIcon class="w-5 h-5" />
+	</h3>
+	<p class="text-sm">
+		Exiting this organization will revoke your access to its feedback data and associated resources.
+		You will no longer be able to view or manage feedback within this organization. If you are the
+		last member in this organization, the organization and all its feedback data will be deleted
+		permanently. This action cannot be reversed.
+	</p>
+	<form method="POST" action="?/leaveOrganization" use:enhance>
+		<button class="btn btn-sm btn-neutral mt-4">Leave "{data.organization?.name}"</button>
+	</form>
+</section>
+
+<section class="mt-8 rounded-lg p-4 bg-gray-100 text-gray-900 dark:bg-gray-950 dark:text-gray-200">
+	<h3 class="flex items-center justify-between text-lg font-medium">
+		Delete the organization!
+		<AlertCircleIcon class="w-5 h-5" />
+	</h3>
+	<p class="text-sm">
+		Deleting this organization will permanently erase all collected feedback data, including
+		associated records and information. Proceeding with this action cannot be undone. Please ensure
+		that you have backed up any critical data or exported necessary information before deleting the
+		organization. This action cannot be reversed.
+	</p>
+	<form method="POST" action="?/deleteOrganization" use:enhance>
+		<button class="btn btn-sm btn-neutral mt-4">Delete "{data.organization?.name}"</button>
+	</form>
+</section>
