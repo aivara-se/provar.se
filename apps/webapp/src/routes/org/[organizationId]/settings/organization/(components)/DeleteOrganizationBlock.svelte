63d8f4944607bcbd3e6ce87a94f3ed01cf5902b3
Thanish Nizam <mnmtanish@gmail.com>
2024-02-17 08:57:10 +0100

    chore: move leave and dialog blocks to components

---
+++ apps/webapp/src/routes/org/[organizationId]/settings/organization/(components)/DeleteOrganizationBlock.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { AlertCircleIcon } from 'lucide-svelte';
+	import { ConfirmModal } from '$lib/client/forms';
+	import type { Organization } from '$lib/types';
+
+	export let organization: Organization;
+
+	let isOpen = false;
+</script>
+
+<section class="mt-8 rounded-lg p-4 bg-gray-100 text-gray-900 dark:bg-gray-950 dark:text-gray-200">
+	<h3 class="flex items-center justify-between text-lg font-medium">
+		Delete the organization!
+		<AlertCircleIcon class="w-5 h-5" />
+	</h3>
+	<p class="text-sm">
+		Deleting this organization will permanently erase all collected feedback data, including
+		associated records and information. Proceeding with this action cannot be undone. Please ensure
+		that you have backed up any critical data or exported necessary information before deleting the
+		organization. This action cannot be reversed.
+	</p>
+	<button class="btn btn-sm btn-neutral mt-4" on:click={() => (isOpen = true)}>
+		Delete {organization?.name}
+	</button>
+	<ConfirmModal
+		bind:isOpen
+		submitPath="?/deleteOrganization"
+		submitText="Delete {organization?.name}"
+	>
+		<AlertCircleIcon class="w-8 h-8" />
+		<p class="py-4 text-center">
+			Are you sure you want to delete {organization?.name}?
+		</p>
+	</ConfirmModal>
+</section>
