31d0e77055114b495185f133edb904515f5393bb
Thanish Nizam <mnmtanish@gmail.com>
2024-02-17 19:51:28 +0100

    fix: fix issues with authjs library

--- apps/webapp/src/routes/+layout.server.ts
+++ apps/webapp/src/routes/+layout.server.ts
@@ -1,14 +1,13 @@
 import { OrganizationRepository } from '$lib/server/organization';
 import { UserRepository } from '$lib/server/user';
-import type { Session } from '@auth/core/types';
 import type { LayoutServerLoad } from './$types';
 
 /**
  * This function is also called when the user is not logged in.
  */
 export const load: LayoutServerLoad = async (event) => {
-	const session = (await event.locals.getSession()) as Session;
-	if (!session) {
+	const session = await event.locals.auth();
+	if (!session || !session.user?.id) {
 		return { session: null, profile: null, organizations: [] };
 	}
 	const [profile, organizations] = await Promise.all([
