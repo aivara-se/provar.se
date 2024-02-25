bac002b95efd8284237a4f9dadc3cb0dad3d2c6e
Thanish Nizam <mnmtanish@gmail.com>
2024-02-25 17:28:39 +0100

    chore: rename some components

---
+++ apps/webapp/src/routes/org/[organizationId]/explore/(components)/SearchAndFilter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { SearchIcon } from 'lucide-svelte';
+</script>
+
+<div class="join flex shadow">
+	<input class="input input-sm join-item flex-1" placeholder="Search ..." />
+	<button class="btn btn-sm join-item">
+		<SearchIcon class="w-3 h-3" />
+	</button>
+</div>
