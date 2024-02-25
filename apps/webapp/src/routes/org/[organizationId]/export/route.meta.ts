420f0d0d60b8e3ea743f35e9cd2da3879e5a6a20
Thanish Nizam <mnmtanish@gmail.com>
2024-02-25 17:02:41 +0100

    feat: add placeholder pages for import and export

---
+++ apps/webapp/src/routes/org/[organizationId]/export/route.meta.ts
@@ -0,0 +1,9 @@
+import { defineRoute } from '$lib/client/routes';
+import parentRoute from '../route.meta';
+
+export default defineRoute({
+	id: 'org-export',
+	parent: parentRoute,
+	getName: () => 'Export',
+	getPath: (params) => `/org/${params.organizationId}/export`
+});
