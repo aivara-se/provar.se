f90fede4b4b5b055387bdab331d56fcbb657192d
Thanish Nizam <mnmtanish@gmail.com>
2024-02-25 08:31:13 +0100

    feat: group projects by the status

--- apps/webapp/src/routes/org/[organizationId]/projects/+page.svelte
+++ apps/webapp/src/routes/org/[organizationId]/projects/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { DashLayout } from '$lib/client/layout';
-	import ProjectsTable from './(components)/ProjectsTable.svelte';
+	import ProjectsList from './(components)/ProjectsList.svelte';
 	import CreateProjectForm from './(forms)/CreateProjectForm.svelte';
 	import route from './route.meta';
 
@@ -8,6 +8,6 @@
 </script>
 
 <DashLayout {route}>
-	<ProjectsTable projects={data.projects} />
+	<ProjectsList projects={data.projects} />
 	<CreateProjectForm formData={data.CreateProjectForm} />
 </DashLayout>
