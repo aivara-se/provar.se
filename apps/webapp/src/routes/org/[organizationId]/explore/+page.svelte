84b68fc4c3d77c5cece84a251145064fedb4c2ba
Thanish Nizam <mnmtanish@gmail.com>
2024-02-25 15:51:50 +0100

    feat: add date range picker user interface

--- apps/webapp/src/routes/org/[organizationId]/explore/+page.svelte
+++ apps/webapp/src/routes/org/[organizationId]/explore/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import { DashLayout } from '$lib/client/layout';
+	import DurationPicker from './(components)/DurationPicker.svelte';
+	import ActionsDropdown from './(components)/ActionsDropdown.svelte';
 	import route from './route.meta';
 </script>
 
 <DashLayout {route}>
-	<p slot="actions">[actions]</p>
+	<section slot="actions" class="flex gap-1 items-center">
+		<DurationPicker />
+		<ActionsDropdown />
+	</section>
+
 	<p>[explore]</p>
 </DashLayout>
