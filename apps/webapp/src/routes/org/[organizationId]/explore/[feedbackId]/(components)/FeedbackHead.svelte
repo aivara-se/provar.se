3be1c61cb407c4b9f7de5a52d72d059160144a0f
Thanish Nizam <mnmtanish@gmail.com>
2024-03-03 10:53:11 +0100

    feat: add feedback details view

---
+++ apps/webapp/src/routes/org/[organizationId]/explore/[feedbackId]/(components)/FeedbackHead.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Feedback } from '$lib/types';
+
+	export let feedback: Feedback;
+</script>
+
+<section class="flex items-center justify-between">
+	<p class="text-xs mt-2 text-gray-500 font-semibold">
+		Created: {feedback.createdAt.toLocaleDateString()} at {feedback.createdAt.toLocaleTimeString()}
+	</p>
+</section>
