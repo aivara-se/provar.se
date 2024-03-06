23fbe6e3b93e911373d91ae016d18739f334cd3c
Thanish Nizam <mnmtanish@gmail.com>
2024-03-06 13:08:36 +0100

    feat: update mockgen library to use the new schema

--- libs/mockgen/src/generators/csat.ts
+++ libs/mockgen/src/generators/csat.ts
@@ -1,21 +1,26 @@
 import { CSAT_DATA } from './csat.data.js';
 
+export type CSATQuestionType = 'rating-11p';
+
 export interface CSATFeedback {
-	csat: number;
-	text?: string;
+	'question-type': CSATQuestionType;
+	'response-data': number;
+	'response-text'?: string;
 }
 
 export function generateCSATFeedback(): CSATFeedback {
+	const responseData = Math.floor(Math.random() * 11);
 	const result: CSATFeedback = {
-		csat: Math.random()
+		'question-type': 'rating-11p',
+		'response-data': responseData
 	};
-	const shouldHaveText = Math.random() > 0.5;
+	const shouldHaveText = Math.random() > 0.25;
 	if (!shouldHaveText) {
 		return result;
 	}
 	const offset = CSAT_DATA.length * (0.25 - Math.random() * 0.5);
-	const indexWithOffset = Math.floor(result.csat * CSAT_DATA.length + offset);
+	const indexWithOffset = Math.floor((responseData / 10) * CSAT_DATA.length + offset);
 	const index = Math.max(0, Math.min(CSAT_DATA.length - 1, indexWithOffset));
-	result.text = CSAT_DATA[index];
+	result['response-text'] = CSAT_DATA[index];
 	return result;
 }
