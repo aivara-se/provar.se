16407f164b2ec9416a12639d6fa1d3bbe028e79b
Thanish Nizam <mnmtanish@gmail.com>
2024-03-06 13:33:11 +0100

    chore: remove boxyhq from the stack (temporary)

--- .scripts/setup-local-env.sh
+++ .scripts/setup-local-env.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 
-# Install additional dependencies
-go install github.com/swaggo/swag/cmd/swag@latest
-
-# Create a key pair for SAML
-mkdir -p apps/account
-openssl req -x509 -newkey rsa:2048 -keyout apps/account/local-saml.pem -out apps/account/local-saml.crt -sha256 -days 365 -nodes -subj "/C=SE/ST=Uppsala/L=Knivsta/O=Security/OU=Operations/CN=provar.se"
-
-# Create a key pair for OpenID
-openssl genrsa -out apps/account/local-openid.pem 3072
-openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in apps/account/local-openid.pem -out apps/account/local-openid.pk8.pem
-openssl rsa -in apps/account/local-openid.pk8.pem -pubout -out apps/account/local-openid.pub.pem
-
-# Create .env file for accounts service
-cat << EOF > apps/account/.env
-DB_ENGINE=mongo
-DB_URL=mongodb://localhost:27017/accounts
-JACKSON_API_KEYS=4X4bPXzeEn7faW1DJ/+CSALMZ4jdZNfn
-NEXTAUTH_URL=http://localhost:5225
-EXTERNAL_URL=http://localhost:5225
-NEXTAUTH_SECRET=ik/egdToHUZTsQ8nbUWjDK0vCziIaj71l/fnNdFICTU=
-NEXTAUTH_ADMIN_CREDENTIALS=admin@provar.se:password
-SAML_AUDIENCE=http://localhost:5225
-PUBLIC_KEY=$(cat apps/account/local-saml.crt | base64)
-PRIVATE_KEY=$(cat apps/account/local-saml.pem | base64)
-OPENID_RSA_PUBLIC_KEY=$(cat apps/account/local-openid.pub.pem | base64)
-OPENID_RSA_PRIVATE_KEY=$(cat apps/account/local-openid.pk8.pem | base64)
-EOF
-
 # Create .env file for webapi service
 cat << EOF > apps/webapi/.env
 MONGO_URI=mongodb://localhost:27017/provar
