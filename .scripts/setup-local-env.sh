8fe5b19bdb4a82089307bdc21e9d6eb4569d5006
Thanish Nizam <mnmtanish@gmail.com>
2024-03-07 10:48:48 +0100

    feat: use secrets if they are available

--- .scripts/setup-local-env.sh
+++ .scripts/setup-local-env.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Source apps/webapi/.secrets file if it exists
+if [ -f apps/webapi/.secrets ]; then
+  source apps/webapi/.secrets
+fi
+
 # Create .env file for webapi service
 cat << EOF > apps/webapi/.env
 AUTH_SECRET=$(openssl rand -base64 40)
 AUTH_EMAIL_FROM=noreply@provar.se
 AUTH_EMAIL_SERVER=smtp://user:pass@localhost:1025
-AUTH_GITHUB_ID=
-AUTH_GITHUB_SECRET=
-AUTH_GOOGLE_ID=
-AUTH_GOOGLE_SECRET=
+AUTH_GITHUB_ID=$APPS_WEBAPI_APP_AUTH_GITHUB_ID
+AUTH_GITHUB_SECRET=$APPS_WEBAPI_APP_AUTH_GITHUB_SECRET
+AUTH_GOOGLE_ID=$APPS_WEBAPI_APP_AUTH_GOOGLE_ID
+AUTH_GOOGLE_SECRET=$APPS_WEBAPI_APP_AUTH_GOOGLE_SECRET
 DATABASE_URI=postgresql://postgres:postgres@localhost/provar?sslmode=disable
 GEOLITE2_DB=./GeoLite2-City.mmdb
 
