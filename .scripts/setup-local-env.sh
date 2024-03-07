c8c00176d470341e4007e5a8dda87c80530b82aa
Thanish Nizam <mnmtanish@gmail.com>
2024-03-07 10:39:34 +0100

    chore: update environment variables on local setup

--- .scripts/setup-local-env.sh
+++ .scripts/setup-local-env.sh
@@ -2,26 +2,26 @@
 
 # Create .env file for webapi service
 cat << EOF > apps/webapi/.env
-MONGO_URI=mongodb://localhost:27017/provar
-GEOLITE2_DB=./GeoLite2-City.mmdb
-PORT=3001
-EOF
-
-# Create .env file for webapp service
-cat << EOF > apps/webapp/.env
+AUTH_SECRET=$(openssl rand -base64 40)
 AUTH_EMAIL_FROM=noreply@provar.se
 AUTH_EMAIL_SERVER=smtp://user:pass@localhost:1025
 AUTH_GITHUB_ID=
 AUTH_GITHUB_SECRET=
 AUTH_GOOGLE_ID=
 AUTH_GOOGLE_SECRET=
-AUTH_SECRET=$(openssl rand -base64 40)
-GCS_IMPORT_BUCKET=provar-imports-dev
-MONGODB_URL=mongodb://localhost:27017/provar
-PROVAR_API_URL=http://localhost:3001
-PUBLIC_PROVAR_APP_URL=http://localhost:5173
-PUBLIC_PROVAR_API_KEY=placeholder
+DATABASE_URI=postgresql://postgres:postgres@localhost/provar?sslmode=disable
+GEOLITE2_DB=./GeoLite2-City.mmdb
 
-# Local only
+# Local only environment variables
 GOOGLE_APPLICATION_CREDENTIALS=../../credentials.json
+PORT=3001
+EOF
+
+# Create .env file for webapp service
+cat << EOF > apps/webapp/.env
+PUBLIC_PROVAR_APP_URL=http://localhost:3002
+PUBLIC_PROVAR_API_URL=http://localhost:3001
+
+# Local only environment variables
+PORT=3002
 EOF
