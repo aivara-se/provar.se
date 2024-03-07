1fa29e0a6193a06273a108396c93e2ff89ca7355
Thanish Nizam <mnmtanish@gmail.com>
2024-03-08 00:56:52 +0100

    chore: use the login endpoint to register

--- apps/webapi/lib/access/login_email.go
+++ apps/webapi/lib/access/login_email.go
@@ -2,5 +2,6 @@ package access
 
 func LoginWithEmail(email string) {
 	// TODO: check whether the user exists in the database
+	// TODO: create a new user if the user does not exist
 	// TODO: send a magic link to the user's email
 }
