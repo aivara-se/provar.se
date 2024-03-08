6a32a55c397e1fa870b59e86071e9cdaf200380e
Thanish Nizam <mnmtanish@gmail.com>
2024-03-08 03:47:50 +0100

    chore: refactor magiclink table

--- .scripts/setup-local-env.sh
+++ .scripts/setup-local-env.sh
@@ -11,12 +11,12 @@ fi
 # Create .env file for webapi service
 cat << EOF > apps/webapi/.env
 AUTH_SECRET=$(openssl rand -base64 40)
-AUTH_EMAIL_FROM=noreply@provar.se
-AUTH_EMAIL_SERVER=smtp://user:pass@localhost:1025
 AUTH_GITHUB_ID=$APPS_WEBAPI_APP_AUTH_GITHUB_ID
 AUTH_GITHUB_SECRET=$APPS_WEBAPI_APP_AUTH_GITHUB_SECRET
 AUTH_GOOGLE_ID=$APPS_WEBAPI_APP_AUTH_GOOGLE_ID
 AUTH_GOOGLE_SECRET=$APPS_WEBAPI_APP_AUTH_GOOGLE_SECRET
+EMAIL_FROM=noreply@provar.se
+EMAIL_SERVER=smtp://user:pass@localhost:1025
 DATABASE_URI=postgresql://postgres:postgres@localhost/provar?sslmode=disable
 GEOLITE2_DB=./GeoLite2-City.mmdb
 
