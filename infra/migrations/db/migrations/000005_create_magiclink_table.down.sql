3bbdf050b79da6ec023f02fe71dd3fa1cd172879
Thanish Nizam <mnmtanish@gmail.com>
2024-03-16 15:23:20 +0100

    feat: add postgres table to hold membership

--- infra/migrations/db/migrations/000005_create_magiclink_table.down.sql
+++ infra/migrations/db/migrations/000005_create_magiclink_table.down.sql
@@ -1,5 +1,5 @@
--- Drop private.magiclink table
-DROP TABLE IF EXISTS private.magiclink CASCADE;
-
 -- Drop public.magiclink view
 DROP VIEW IF EXISTS public.magiclink CASCADE;
+
+-- Drop private.magiclink table
+DROP TABLE IF EXISTS private.magiclink CASCADE;
---
+++ infra/migrations/db/migrations/000007_create_organizationmember.down.sql
@@ -0,0 +1,5 @@
+-- Drop public.organizationmember view
+DROP VIEW IF EXISTS public.organizationmember CASCADE;
+
+-- Drop private.organizationmember table
+DROP TABLE IF EXISTS private.organizationmember CASCADE;
