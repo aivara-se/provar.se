fb27ae78cf04a08e5768d74a8aa08f740b3d4a0d
Thanish Nizam <mnmtanish@gmail.com>
2024-03-23 17:55:36 +0100

    feat: add export page and onboard page

---
+++ apps/webapp/src/routes/org/[slug]/onboard/route.meta.ts
@@ -0,0 +1,9 @@
+import { defineRoute } from '$lib/client/routes';
+import parentRoute from '../route.meta';
+
+export default defineRoute({
+	id: 'org-onboard',
+	parent: parentRoute,
+	getName: () => 'Onboarding',
+	getPath: (params) => `/org/${params.slug}/onboard`
+});
