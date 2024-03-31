09bd64a5eb660ff8db21f53cb1e0a967f6864467
Thanish Nizam <mnmtanish@gmail.com>
2024-03-31 23:40:06 +0200

    chore: simplify permissions, all users are admins

--- apps/webapi/api/invitation/delete_invitation.go
+++ apps/webapi/api/invitation/delete_invitation.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"provar.se/webapi/lib/access"
 	"provar.se/webapi/lib/invitation"
-	"provar.se/webapi/lib/permission"
+	"provar.se/webapi/lib/organization"
 	"provar.se/webapi/lib/router"
 )
 
@@ -12,12 +12,8 @@ func SetupDeleteInvitation(app *fiber.App) {
 	path := "/organization/:organizationId/invitation/:invitationId"
 
 	app.Delete(path, access.AuthenticatedGuard())
-	app.Delete(path, access.PermissionGuard(&access.PermissionGuardOptions{
-		OrganizationID: router.FromPathParam("organizationId"),
-		ResourceType:   permission.ResourceTypeOrganization,
-		ResourceID:     router.FromPathParam("organizationId"),
-		Permission:     permission.PermissionTypeOrganizationAdmin,
-	}))
+	app.Delete(path, organization.Loader(router.FromPathParam("organizationId")))
+	app.Delete(path, access.MembershipGuard())
 
 	app.Delete(path, func(c *fiber.Ctx) error {
 		inviteID := c.Params("invitationId")
