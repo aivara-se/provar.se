b81b3249ff41febe512e6225d595e9963c9b86b4
Thanish Nizam <mnmtanish@gmail.com>
2024-04-01 02:09:35 +0200

    feat: create table to store feedback data

---
+++ infra/migrations/db/migrations/000010_create_feedback_table.up.sql
@@ -0,0 +1,16 @@
+-- Create private.feedback table
+CREATE TABLE private.feedback (
+    id VARCHAR(8) PRIMARY KEY,
+    organization_id VARCHAR(8) REFERENCES private.organization(id) ON DELETE CASCADE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    question_type VARCHAR(32) NOT NULL,
+    feedback_type VARCHAR(32) NOT NULL,
+    feedback_time TIMESTAMP NOT NULL,
+    feedback_data JSONB NOT NULL,
+    feedback_tags JSONB NOT NULL,
+    feedback_meta JSONB NOT NULL,
+    feedback_user JSONB NOT NULL
+);
+
+-- Index on organization_id column of private.feedback table
+CREATE INDEX idx_feedback_organization_id ON private.feedback(organization_id);
