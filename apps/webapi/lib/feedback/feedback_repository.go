643a8f4ef2dc91b814608eb7a632feb1700dfa47
Thanish Nizam <mnmtanish@gmail.com>
2024-04-01 14:23:24 +0200

    feat: add repository code to query feedback data

--- apps/webapi/lib/feedback/feedback_repository.go
+++ apps/webapi/lib/feedback/feedback_repository.go
@@ -1,6 +1,7 @@
 package feedback
 
 import (
+	"fmt"
 	"time"
 
 	"provar.se/webapi/lib/database"
@@ -29,19 +30,30 @@ type Feedback struct {
 	FeedbackUser   map[string]string `db:"feedback_user" json:"feedbackUser"`
 }
 
+// CreateFeedbackData represents the data needed to create a new feedback
+type CreateFeedbackData struct {
+	QuestionType string
+	FeedbackTime time.Time
+	FeedbackType FeedbackType
+	FeedbackData map[string]string
+	FeedbackTags map[string]string
+	FeedbackMeta map[string]string
+	FeedbackUser map[string]string
+}
+
 // Create creates a new feedback in the database
-func Create(organizationID string, questionType string, feedbackTime time.Time, feedbackType FeedbackType, feedbackData map[string]string, feedbackTags map[string]string, feedbackMeta map[string]string, feedbackUser map[string]string) (*Feedback, error) {
+func Create(organizationID string, data *CreateFeedbackData) (*Feedback, error) {
 	fb := &Feedback{
 		ID:             database.NewID(),
 		OrganizationID: organizationID,
 		CreatedAt:      time.Now(),
-		QuestionType:   questionType,
-		FeedbackTime:   feedbackTime,
-		FeedbackType:   feedbackType,
-		FeedbackData:   feedbackData,
-		FeedbackTags:   feedbackTags,
-		FeedbackMeta:   feedbackMeta,
-		FeedbackUser:   feedbackUser,
+		QuestionType:   data.QuestionType,
+		FeedbackTime:   data.FeedbackTime,
+		FeedbackType:   data.FeedbackType,
+		FeedbackData:   data.FeedbackData,
+		FeedbackTags:   data.FeedbackTags,
+		FeedbackMeta:   data.FeedbackMeta,
+		FeedbackUser:   data.FeedbackUser,
 	}
 	query := `
 		INSERT INTO private.feedback (id, organization_id, created_at, question_type, feedback_time, feedback_type, feedback_data, feedback_tags, feedback_meta, feedback_user)
@@ -49,6 +61,65 @@ func Create(organizationID string, questionType string, feedbackTime time.Time, 
 	`
 	_, err := database.DB().NamedExec(query, fb)
 	return fb, err
+}
+
+// SearchFeedbackData represents the data needed to search for feedback
+type SearchFeedbackData struct {
+	BegTimestamp time.Time
+	EndTimestamp time.Time
+	QuestionType string
+	FeedbackType FeedbackType
+	FeedbackTags map[string]string
+	FeedbackMeta map[string]string
+	FeedbackUser map[string]string
+}
+
+// Search searches for feedback in the database with given data
+func Search(organizationID string, data *SearchFeedbackData) ([]*Feedback, error) {
+	feedbacks := []*Feedback{}
+	query := "SELECT * FROM private.feedback WHERE organization_id = $1"
+	argv := []interface{}{organizationID}
+	argc := 1
+	if !data.BegTimestamp.IsZero() {
+		query += " AND created_at >= $" + fmt.Sprint(argc)
+		argv = append(argv, data.BegTimestamp)
+		argc++
+	}
+	if !data.EndTimestamp.IsZero() {
+		query += " AND created_at <= $" + fmt.Sprint(argc)
+		argv = append(argv, data.EndTimestamp)
+		argc++
+	}
+	if data.QuestionType != "" {
+		query += " AND question_type = $" + fmt.Sprint(argc)
+		argv = append(argv, data.QuestionType)
+		argc++
+	}
+	if data.FeedbackType != "" {
+		query += " AND feedback_type = $" + fmt.Sprint(argc)
+		argv = append(argv, data.FeedbackType)
+		argc++
+	}
+	for k, v := range data.FeedbackTags {
+		query += " AND feedback_tags->>$" + fmt.Sprint(argc) + " = $" + fmt.Sprint(argc+1)
+		argv = append(argv, k, v)
+		argc += 2
+	}
+	for k, v := range data.FeedbackMeta {
+		query += " AND feedback_meta->>$" + fmt.Sprint(argc) + " = $" + fmt.Sprint(argc+1)
+		argv = append(argv, k, v)
+		argc += 2
+	}
+	for k, v := range data.FeedbackUser {
+		query += " AND feedback_user->>$" + fmt.Sprint(argc) + " = $" + fmt.Sprint(argc+1)
+		argv = append(argv, k, v)
+		argc += 2
+	}
+	err := database.DB().Select(&feedbacks, query, argv...)
+	if err != nil {
+		return nil, err
+	}
+	return feedbacks, nil
 }
 
 // FindByID returns a feedback with the given id
