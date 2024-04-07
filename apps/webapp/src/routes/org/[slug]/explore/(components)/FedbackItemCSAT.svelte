0cbdad10cd89bf240c074888c473d7850478bd1c
Thanish Nizam <mnmtanish@gmail.com>
2024-04-07 03:04:53 +0200

    feat: add message when text is missing

--- apps/webapp/src/routes/org/[slug]/explore/(components)/FedbackItemCSAT.svelte
+++ apps/webapp/src/routes/org/[slug]/explore/(components)/FedbackItemCSAT.svelte
@@ -6,7 +6,11 @@
 </script>
 
 <span class="text-xs font-semibold absolute top-2 right-4 opacity-50">CSAT</span>
-<p>{feedback.feedbackData['response-text']}</p>
+{#if feedback.feedbackData['response-text']}
+	<p>{feedback.feedbackData['response-text']}</p>
+{:else}
+	<p class="italic opacity-50">Feedback text not available</p>
+{/if}
 <p class="mt-1 text-xs opacity-50">
 	{format(feedback.createdAt, 'yyyy-MM-dd')} at {format(feedback.createdAt, 'HH:mm')}
 </p>
