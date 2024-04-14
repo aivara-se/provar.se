9e9bb2fc22ac8e164fa7fb2ec36c30fdf0c23824
Thanish Nizam <mnmtanish@gmail.com>
2024-04-14 04:51:39 +0200

    chore: remove onboard placeholder page

--- apps/webapp/src/routes/org/[slug]/+page.ts
+++ apps/webapp/src/routes/org/[slug]/+page.ts
@@ -3,7 +3,5 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async (event) => {
 	const { organization } = await event.parent();
-	// TODO: redirect to `/org/${organization.slug}/onboard`
-	// redirect(302, `/org/${organization.slug}/explore`);
-	redirect(302, `/org/${organization.slug}/onboard`);
+	redirect(302, `/org/${organization.slug}/explore`);
 };
