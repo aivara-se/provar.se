7df5fc7fd4748e40b689b7cfa47f4a5ed4a1b83c
Thanish Nizam <mnmtanish@gmail.com>
2025-01-19 18:26:51 +0100

    chore: ignore built files and generated files

--- libs/provar-js/eslint.config.js
+++ libs/provar-js/eslint.config.js
@@ -2,8 +2,7 @@ import * as preslint from '@provar/eslint-config';
 
 export default [
 	...preslint.typescript(),
-	// Add more configs here
-	{
-		ignores: ['src/api.type.ts', 'src/api.code.ts']
-	}
+
+	// Ignore auto-generated files and build files from linting
+	{ ignores: ['src/api.type.ts', 'src/api.code.ts', 'build/*'] }
 ];
